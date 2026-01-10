944ac9b838b30d1e40f9687cc57cf2f325df6ee5
Aurif <filmosyt@gmail.com>
2026-01-10 19:23:56 +0100

    Added laser path tracing

--- components/mol/laser/laser.gd
+++ components/mol/laser/laser.gd
@@ -24,6 +24,8 @@ func _physics_process(delta):
 
     if is_on_wall():
         direction *= -1
+        
+    _update_laser_path()
 
 ###
 ### Firing handling
@@ -47,7 +49,6 @@ func _input(event: InputEvent) -> void:
      and InputMap.event_is_action(event, "switch_angle") \
      and (not event.echo or Time.get_ticks_msec() > sweep_last_exec + SWEEP_COOLDOWN):
         sweep_last_exec = Time.get_ticks_msec()
-        print("W")
         switch_angle()
 
 ###
@@ -62,14 +63,40 @@ func switch_angle() -> void:
     %LaserLine.rotation_degrees = VALID_ANGLES[current_angle]
 
 ###
+### Path prediction
+###
+func _update_laser_path():
+    %RayCast.clear_exceptions()
+    %RayCast.position = %LaserLine.position
+    %RayCast.rotation_degrees = %LaserLine.rotation_degrees
+    var laser_path: Array[Vector2i] = [Vector2i.ZERO]
+    
+    for i in range(100):
+        %RayCast.force_raycast_update()
+        laser_path.append(Vector2i(%LaserLine.to_local(%RayCast.get_collision_point())))
+        %RayCast.global_position = %RayCast.get_collision_point()
+        
+        var incident: Vector2 = %RayCast.target_position.rotated($RayCast.global_rotation).normalized()
+        var normal: Vector2 = %RayCast.get_collision_normal()
+        var reflected: Vector2 = incident - 2 * incident.dot(normal) * normal
+        %RayCast.rotation = reflected.angle()
+        
+        if %RayCast.get_collider().name == 'WorldBorder':
+            break
+        %RayCast.clear_exceptions()
+        %RayCast.add_exception(%RayCast.get_collider())
+        
+    %LaserLine.points = laser_path
+
+###
 ### Active animation
 ###
 var tween: Tween
 func _handle_active_change():
     if not is_active and tween:
-        tween.stop()
+        tween.kill()
         tween = null
-        self.modulate = Color.WHITE
+        modulate_target.modulate = Color.WHITE
         
     if is_active and not tween:
         if not is_inside_tree():
